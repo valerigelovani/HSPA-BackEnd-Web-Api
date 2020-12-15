9f5b3073dcccadef214063e30e689f6df882e1ad
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-12-15 14:06:33 +0400

    MakeNameRequired

--- Dtos/CityDto.cs
+++ Dtos/CityDto.cs
@@ -9,6 +9,7 @@ namespace HSPA_Web_Api.Dtos
     public class CityDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         [Required]
         public string Country { get; set; }
