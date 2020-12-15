da1804519b526107d6d9228f6de4f4418c7b4939
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-12-15 17:22:55 +0400

    HandlingErrorsGloballyCreateExtensionForErrorHandling

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using HSPA_Web_Api.Data;
+using HSPA_Web_Api.Extensions;
 using HSPA_Web_Api.Helpers;
 using HSPA_Web_Api.Interfaces;
 using Microsoft.AspNetCore.Builder;
@@ -37,28 +38,7 @@ namespace HSPA_Web_Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage(); // detaluri exception ebi aqedan modis
-            }
-            else 
-            {
-                app.UseExceptionHandler( // tu rame cnobili exception iqneba zemot ^ mouvlis ifshi is metodi,  danarchen unknow erors aq gavfiltravt
-                    options => {
-                        options.Run(
-                            async context =>
-                            {
-                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                                var ex = context.Features.Get<IExceptionHandlerFeature>();
-                                if (ex != null)
-                                {
-                                    await context.Response.WriteAsync(ex.Error.Message);
-                                }
-                            }
-                           );
-                    }
-                    );
-            }
+            app.ConfigureExceptionHandler(env);
 
             app.UseRouting();
             app.UseCors(m => m.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //crosed core error fix amis gareshe erors amoagdeb saitze radgan ori localhostia
