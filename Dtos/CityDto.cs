8dbb236b1db1425550d3bde40fe43cb36ff050fe
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-12-15 14:01:21 +0400

    InputValidationAndExceptionHandlingPrt1

--- Dtos/CityDto.cs
+++ Dtos/CityDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace HSPA_Web_Api.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [Required]
         public string Country { get; set; }
     }
 }
--- Models/City.cs
+++ Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace HSPA_Web_Api.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [Required]
         public string Country { get; set; }
 
         public DateTime LastUpdatedOn { get; set; }
