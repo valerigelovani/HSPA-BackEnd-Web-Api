c7b60658b6b1873efe960e54af4691187c85bcba
Valeri Gelovani <valeri.gelovani@yahoo.com>
2020-12-15 17:03:32 +0400

    UnknownErrorsHandlingGlobally

--- Startup.cs
+++ Startup.cs
@@ -3,11 +3,14 @@ using HSPA_Web_Api.Data;
 using HSPA_Web_Api.Helpers;
 using HSPA_Web_Api.Interfaces;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net;
 
 namespace HSPA_Web_Api
 {
@@ -37,6 +40,24 @@ namespace HSPA_Web_Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage(); // detaluri exception ebi aqedan modis
+            }
+            else 
+            {
+                app.UseExceptionHandler( // tu rame cnobili exception iqneba zemot ^ mouvlis ifshi is metodi,  danarchen unknow erors aq gavfiltravt
+                    options => {
+                        options.Run(
+                            async context =>
+                            {
+                                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                var ex = context.Features.Get<IExceptionHandlerFeature>();
+                                if (ex != null)
+                                {
+                                    await context.Response.WriteAsync(ex.Error.Message);
+                                }
+                            }
+                           );
+                    }
+                    );
             }
 
             app.UseRouting();
